17db8ce7eaba66939f028a9dd318ffef438c7373
AndresG <gazuiandres@gmail.com>
2020-09-22 11:07:47 -0400

    preparando producto

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['title', 'description', 'price', 'image', 'category_id'];
+    protected $fillable = ['title', 'description', 'price', 'image', 'category_id', 'slug'];
 
 
     public function category()
--- database/migrations/2020_09_22_150115_create_product_images_table.php
+++ database/migrations/2020_09_22_150115_create_product_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateProductImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('slug');
-            $table->text('description');
-            $table->integer('price');
             $table->string('image');
-            $table->foreignId('category_id');
+            $table->foreignId('product_id');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('product_images');
     }
 }
