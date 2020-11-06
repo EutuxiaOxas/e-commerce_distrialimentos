b641ef33720abfa6277d8e762e6e97209d43da85
AndresG <gazuiandres@gmail.com>
2020-11-06 17:41:43 -0400

    cambios

--- resources/views/cms/layout/main.blade.php
+++ resources/views/cms/layout/main.blade.php
@@ -158,9 +158,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{route('cms.home')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+    <a href="{{route('cms.home')}}" class="brand-link">
       <span class="brand-text font-weight-light">Administrador</span>
     </a>
 
@@ -171,7 +169,7 @@
         <div class="image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{auth()->user()->name}}</a>
+          <a href="#" class="d-block">Usuario: {{auth()->user()->name}}</a>
         </div>
       </div>
 
@@ -180,35 +178,6 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li>
           @if(auth()->user()->roles->title == 'administrador')
           <li class="nav-item ">
             <a href="{{ route('cms.users') }}" class="nav-link secciones usuarios ">
