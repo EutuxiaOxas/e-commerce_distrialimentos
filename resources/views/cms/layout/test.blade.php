57b798189c99b75ca77bbc95213e35a3f0dbfe4f
AndresG <gazuiandres@gmail.com>
2020-11-06 16:37:33 -0400

    plantilla administracion productos

--- resources/views/cms/layout/test.blade.php
+++ resources/views/cms/layout/test.blade.php
@@ -220,6 +220,29 @@
               </li>
             </ul>
           </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Tienda
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{route('tienda.category.home')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Categorias</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{route('tienda.product.home')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Productos</p>
+                </a>
+              </li>
+            </ul>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
