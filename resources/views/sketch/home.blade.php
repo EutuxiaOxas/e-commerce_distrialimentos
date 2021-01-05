373fe14c6213a506f2d9ecbae07920d5c1426a68
Mariana Lamus <marilams995@gmail.com>
2021-01-05 17:08:07 -0400

    home

--- resources/views/sketch/home.blade.php
+++ resources/views/sketch/home.blade.php
@@ -1,7 +1,7 @@
 @extends('common.main')
 
 @section('title')
-    <title>Nosotros</title>
+    <title>Distrialimentos</title>
 @endsection
 
 @php
@@ -64,10 +64,10 @@ $color_header='dark';
       </div>
     </div>
   </section>
-
+<!-- //productos destacados -->
   <!-- categorias -->
   <section>
-  <div class="container">
+    <div class="container">
     <h2>Categoria de productos</h2>
       <div class="row mb-0 py-0">
           <div class="col-6 col-md-3">
@@ -171,9 +171,8 @@ $color_header='dark';
     </div>
   </div>
   </div>
-
-  </section>
-  
+</section>
+  <!-- /categorias -->
  
 
 
@@ -219,152 +218,9 @@ $color_header='dark';
     </div>
   </section>
   <!-- / cover -->
- <!-- presentation -->
- <section class="section-lg">
-   <div class="container">
-     <div class="row my-10">
-       <div class="col-lg-6">
-         <div class="row">
-           <div class="col">
-            <h2>Los <b>Beneficios</b> <br> para tu establecimiento.</h2>
-           </div>
-         </div>
-         <div class="row gutter-3">
+ 
 
-          <div class="col-md-6" data-aos="fade-up">
-            <div class="media">
-              <i class="icon-check-circle2 fs-30 text-primary mr-2"></i>
-              <div class="media-body">
-                <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Productos</h5>
-                <p>Tenemos como aliados a las principales marcas más reconocidas del país.</p>
-              </div>
-            </div>
-          </div>
-
-           <div class="col-md-6" data-aos="fade-up">
-             <div class="media">
-               <i class="icon-laptop fs-30 text-primary mr-2"></i>
-               <div class="media-body">
-                 <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Canales</h5>
-                 <p>Toma de los pedidos vía online, whatsapp Business, o presencial con nuestros vendedores.</p>
-                </div>
-             </div>
-           </div>
-           <div class="col-md-6" data-aos="fade-up">
-             <div class="media">
-               <i class="icon-truck fs-30 text-primary mr-2"></i>
-               <div class="media-body">
-                 <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Despacho</h5>
-                 <p>Despacho garantizado a la puerta de su establecimiento en un plazo entre 48 y 72 horas.</p>
-               </div>
-             </div>
-            </div>
-            <div class="col-md-6" data-aos="fade-up">
-              <div class="media">
-                <i class="icon-user2 fs-30 text-primary mr-2"></i>
-                <div class="media-body">
-                  <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Vendedores</h5>
-                  <p>Atención de nuestros vendedores en sus establecimientos si así lo desean.</p>
-                </div>
-              </div>
-            </div>
-           
-         </div>
-       </div>
-       <div class="col-12 col-lg-6 presentation presentation-responsive d-none d-md-block">
-         <img class="left-0 vertical-align" src="{{asset('images/cta/camion.jpg')}}" alt="Image">
-       </div>
-     </div>
-   </div>
- </section>
- <!-- / presentation -->
-
- <!-- faq -->
- <section class="bg-light separator-top">
-   <div class="container">
-     <div class="row justify-content-between align-items-center">
-       <div class="col-md-6 pr-md-4">
-        
-         <div class="accordion-group accordion-group-highlight" data-accordion-group>
-           <div class="accordion open" data-accordion data-aos="fade-up">
-             <div class="accordion-control" data-control>
-               <h5>QUIENES SOMOS</h5>
-             </div>
-             <div class="accordion-content" data-content>
-               <div class="accordion-content-wrapper">
-                  <p>Somos una empresa distribuidora de consumo masivo, fundada bajo los cimientos de la experiencia en el sector, producto de más de seis (6) años de trabajo con otras empresas especializadas en la distribución e importación de productos en el área del Food Service, para cadenas de Supermercados, hipermercados, mini-mercados, panaderías, carnicerías, bodegones, restaurantes y otras empresas de la industria. </p> 
-               </div>
-             </div>
-           </div>
-           <div class="accordion" data-accordion data-aos="fade-up">
-             <div class="accordion-control" data-control>
-               <h5>MISIÓN</h5>
-             </div>
-             <div class="accordion-content" data-content>
-               <div class="accordion-content-wrapper">
-                 <p>Contribuir y aportar al mercado venezolano variedad y calidad de productos de alto valor, fortaleciendo nuestro estilo de negocio, distribución y venta apoyándonos con marcas de reconocimiento nacional e internacional en el área de consumo masivo. </p>
-               </div>
-             </div>
-           </div>
-           <div class="accordion" data-accordion data-aos="fade-up">
-             <div class="accordion-control" data-control>
-               <h5>VISIÓN</h5>
-             </div>
-             <div class="accordion-content" data-content>
-               <div class="accordion-content-wrapper">
-                 <p>Convertirnos en una de las principales compañías de distribución e importación de consumo masivo de alta calidad, para así obtener presencia en todo el territorio nacional, fortaleciendo lazos con nuestros proveedores a nivel nacional e internacional, con nuevas marcas en mercado nacional. </p>
-               </div>
-             </div>
-           </div>
-           <div class="accordion" data-accordion data-aos="fade-up">
-            <div class="accordion-control" data-control>
-              <h5>LINEAS DE PRODUCTOS</h5>
-            </div>
-            <div class="accordion-content" data-content>
-              <div class="accordion-content-wrapper">
-                <p>Nos dedicamos a la distribución de alimentos para el consumo humano y del hogar tales como: Víveres, Lácteos, Avena y cereales, Pastas, harinas y azucares, Granos, Enlatados, Bebidas, Condimentos, Galletas, tortas y ponqués, Especies, Aceites y mantequillas, Snack, Higiene personal, Cuidado y mantenimientos del hogar.</p>
-              </div>
-            </div>
-          </div>
-          <div class="accordion" data-accordion data-aos="fade-up">
-            <div class="accordion-control" data-control>
-              <h5>DISTRIBUCIÓN</h5>
-            </div>
-            <div class="accordion-content" data-content>
-              <div class="accordion-content-wrapper">
-                <p>Actualmente estamos en la zona Centro – Occidente del país, distribuyendo en los estados Carabobo, Aragua, Falcón y próximamente en otros estados del territorio nacional, contamos con un equipo de vendedores capacitados para atender las necesidades de nuestros clientes.</p>
-              </div>
-            </div>
-          </div>
-
-
-         </div>
-       </div>
-       <div class="col-md-6">
-         <img src="{{asset('images/portada/logo-full.png')}}" alt="Image">
-         {{-- <h2><b>Distrialimentos del centro</b></h2> --}}
-         <h3  class="mb-4">Trabajamos para tu comodidad y la de tu familia.</h3>
-       </div>
-     </div>
-   </div>
- </section>
- <!-- / faq -->
-
-<section class="bg-light pt-0">
-   <div class="container">
-     <div class="row align-items-center">
-       <div class="col-md-6 pr-md-5 d-none d-md-block">
-         <img src="{{asset('images/cta/clientes.png')}}" alt="Image">
-       </div>
-     
-       <div class="col-md-6 pr-md-4">
-        <h2><b>Nuestro proposito</b></h2>
-        <h5  class="mb-4">Llegar a cada rincón de Venezuela, con los estándares más altos de calidad en la comercialización y distribución de productos consumo masivo.</h5>
-       </div>
-    </div>
-  </div>
-</section>
-
+ 
  <!-- blog -->
  <section class="bg-light">
    <div class="container">
