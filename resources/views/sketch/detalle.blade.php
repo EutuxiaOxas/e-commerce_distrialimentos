9eeb5a96ec1ae646f36262497d944811f5a87a3c
Mariana Lamus <marilams995@gmail.com>
2021-01-08 15:18:49 -0400

    vista desktop

--- resources/views/sketch/detalle.blade.php
+++ resources/views/sketch/detalle.blade.php
@@ -25,6 +25,21 @@
    .mart { /* margen separacion vista movil */
      margin-top: 6px;
    }
+   @media(min-width: 767px){
+     .arg { /* padding fila inferior desktop */
+       padding-right: 0 !important;
+       padding-left: 0 !important;
+     }
+     .tl { /* centrado precio desktop */
+      text-align: left !important;
+     }
+     .centr { /* centrado tarjeta desktop */
+      padding-bottom: 10px;
+      padding-top: 5px;
+      padding-left: 10px;
+     }
+     
+   }
    </style>
    <!-- Prdoduct -->
     <section class="pb-3">
@@ -72,41 +87,152 @@
    <section class="p-0">
     <div class="container">
       <h3 class="text-primary">Productos relacionados</h3>
-      <div class="row px-2 mb-1">
+      <div class="row px-2 mb-1"> <!--  tarjeta-body-->
         <div class="col p-0">
           <div class="row boxed border shadow p-0">
             <div class="col-5 col-md-3 p-0">
               <img src="{{asset('images/lineas/linea-viveres.jpg')}}" alt="Product-related">
             </div>
             <div class="col-7 col-md-9 p-0">
-              <h5 class="text-blue font-weight-bold px-1 pt-1 pb-0 m-0">Titulo del producto</h5>
-              <p class="small px-1 m-0 pb-0">Descripcion del producto</p>
+              <div class="prod-details centr">
+                <div class="row m-0">
+                    <div class="col-12 col-md-6 m-0 p-0">
+                      <h5 class="text-blue font-weight-bold px-1 pt-1 pb-0 m-0">Titulo del producto</h5>
+                    </div>
+                    <div class="col-6 d-none d-md-block">
+                      <p class="text-muted text-right smaller pt-1 pr-2 mb-0"><strong class="text-muted">SKU:</strong>00005644545</p>
+                    </div>
+                </div>
+                <p class="small px-1 m-0 pb-0">Descripcion del producto</p>
                 <div class="row p-0 m-0">
-                  <div class="col-7 ol-md-12 pl-1 pb-0 m-0">
-                    <p class="text-card">(24 disponibles)</p>
-                  </div>
-                  <div class="d-none d-md-block pl-1 col-md-12">
-                  <a href="#" class="smaller marb">Categoria</a>
-                  </div>
-                  <div class="col-5 col-md-12 pl-0 m-0">
-                    <p class="smaller pl-1">IVA incluido</p>
-                  </div>
+                    <div class="col-7 col-md-12 pl-1 pb-0 m-0">
+                        <p class="text-card">(24 disponibles)</p>
+                    </div>
+                    <div class="d-none d-md-block pl-1 col-md-12">
+                       <a href="#" class="smaller marb">Categoria</a>
+                    </div>
+                    <div class="col-5 col-md-12 pl-0 m-0">
+                       <p class="smaller pl-1">IVA incluido</p>
+                    </div>
                 </div>
-              <p class="smaller px-1 m-0 font-weight-bold text-black d-block d-md-none">2,00 $ / Undidad</p>
-                <div class="row px-4 my-0 padt">
-                  <div class="col-10 col-md-8 p-0 m-0">
-                    <p class="smaller px-1 fs-18 mb-0 font-weight-bold text-black text-center">20,00 $</p>
-                    <p class="text-card text-center">Caja - 30 unidades</p>
-                  </div>
-                  <div class="col-2 col-md-4 p-0 m-0">
-                  <a href="#" class="btn btn-primary d-none d-md-inline-block px-3 py-1">Agregar</a>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="d-md-none mart font-weight-bold bi bi-plus-circle text-primary align-center" viewBox="0 0 16 16">
-                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                    </svg>          
-                  </div>
+                <p class="smaller px-1 m-0 font-weight-bold text-black d-block d-md-none">2,00 $ / Undidad</p>
+                <div class="row px-4 my-0 padt arg">
+                    <div class="col-md-4 d-none d-md-block">
+                      <p class="smaller px-1 m-0 font-weight-bold text-black">2,00 $ / Undidad</p>
+                    </div>
+                    <div class="col-10 text-center col-md-4 tl p-0 m-0">
+                      <p class="smaller px-1 fs-18 mb-0 font-weight-bold text-black">20,00 $</p>
+                      <p class="text-card">Caja - 30 unidades</p>
+                    </div>
+                    <div class="col-2 col-md-4 p-0 m-0">
+                      <a href="#" class="btn btn-primary d-none d-md-inline-block px-4 py-1">Agregar</a>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="d-md-none mart font-weight-bold bi bi-plus-circle text-primary align-center" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                       </svg>          
+                    </div>
                 </div>
-
+              </div>
+            </div>        
+          </div>
+        </div>
+      </div>
+      <div class="row px-2 mb-1"> <!--  tarjeta-body-->
+        <div class="col p-0">
+          <div class="row boxed border shadow p-0">
+            <div class="col-5 col-md-3 p-0">
+              <img src="{{asset('images/lineas/linea-viveres.jpg')}}" alt="Product-related">
+            </div>
+            <div class="col-7 col-md-9 p-0">
+              <div class="prod-details centr">
+                <div class="row m-0">
+                    <div class="col-12 col-md-6 m-0 p-0">
+                      <h5 class="text-blue font-weight-bold px-1 pt-1 pb-0 m-0">Titulo del producto</h5>
+                    </div>
+                    <div class="col-6 d-none d-md-block">
+                      <p class="text-muted text-right smaller pt-1 pr-2 mb-0"><strong class="text-muted">SKU:</strong>00005644545</p>
+                    </div>
+                </div>
+                <p class="small px-1 m-0 pb-0">Descripcion del producto</p>
+                <div class="row p-0 m-0">
+                    <div class="col-7 col-md-12 pl-1 pb-0 m-0">
+                        <p class="text-card">(24 disponibles)</p>
+                    </div>
+                    <div class="d-none d-md-block pl-1 col-md-12">
+                       <a href="#" class="smaller marb">Categoria</a>
+                    </div>
+                    <div class="col-5 col-md-12 pl-0 m-0">
+                       <p class="smaller pl-1">IVA incluido</p>
+                    </div>
+                </div>
+                <p class="smaller px-1 m-0 font-weight-bold text-black d-block d-md-none">2,00 $ / Undidad</p>
+                <div class="row px-4 my-0 padt arg">
+                    <div class="col-md-4 d-none d-md-block">
+                      <p class="smaller px-1 m-0 font-weight-bold text-black">2,00 $ / Undidad</p>
+                    </div>
+                    <div class="col-10 text-center col-md-4 tl p-0 m-0">
+                      <p class="smaller px-1 fs-18 mb-0 font-weight-bold text-black">20,00 $</p>
+                      <p class="text-card">Caja - 30 unidades</p>
+                    </div>
+                    <div class="col-2 col-md-4 p-0 m-0">
+                      <a href="#" class="btn btn-primary d-none d-md-inline-block px-4 py-1">Agregar</a>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="d-md-none mart font-weight-bold bi bi-plus-circle text-primary align-center" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                       </svg>          
+                    </div>
+                </div>
+              </div>
+            </div>        
+          </div>
+        </div>
+      </div>
+      <div class="row px-2 mb-1"> <!--  tarjeta-body-->
+        <div class="col p-0">
+          <div class="row boxed border shadow p-0">
+            <div class="col-5 col-md-3 p-0">
+              <img src="{{asset('images/lineas/linea-viveres.jpg')}}" alt="Product-related">
+            </div>
+            <div class="col-7 col-md-9 p-0">
+              <div class="prod-details centr">
+                <div class="row m-0">
+                    <div class="col-12 col-md-6 m-0 p-0">
+                      <h5 class="text-blue font-weight-bold px-1 pt-1 pb-0 m-0">Titulo del producto</h5>
+                    </div>
+                    <div class="col-6 d-none d-md-block">
+                      <p class="text-muted text-right smaller pt-1 pr-2 mb-0"><strong class="text-muted">SKU:</strong>00005644545</p>
+                    </div>
+                </div>
+                <p class="small px-1 m-0 pb-0">Descripcion del producto</p>
+                <div class="row p-0 m-0">
+                    <div class="col-7 col-md-12 pl-1 pb-0 m-0">
+                        <p class="text-card">(24 disponibles)</p>
+                    </div>
+                    <div class="d-none d-md-block pl-1 col-md-12">
+                       <a href="#" class="smaller marb">Categoria</a>
+                    </div>
+                    <div class="col-5 col-md-12 pl-0 m-0">
+                       <p class="smaller pl-1">IVA incluido</p>
+                    </div>
+                </div>
+                <p class="smaller px-1 m-0 font-weight-bold text-black d-block d-md-none">2,00 $ / Undidad</p>
+                <div class="row px-4 my-0 padt arg">
+                    <div class="col-md-4 d-none d-md-block">
+                      <p class="smaller px-1 m-0 font-weight-bold text-black">2,00 $ / Undidad</p>
+                    </div>
+                    <div class="col-10 text-center col-md-4 tl p-0 m-0">
+                      <p class="smaller px-1 fs-18 mb-0 font-weight-bold text-black">20,00 $</p>
+                      <p class="text-card">Caja - 30 unidades</p>
+                    </div>
+                    <div class="col-2 col-md-4 p-0 m-0">
+                      <a href="#" class="btn btn-primary d-none d-md-inline-block px-4 py-1">Agregar</a>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="d-md-none mart font-weight-bold bi bi-plus-circle text-primary align-center" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                       </svg>          
+                    </div>
+                </div>
+              </div>
             </div>        
           </div>
         </div>
