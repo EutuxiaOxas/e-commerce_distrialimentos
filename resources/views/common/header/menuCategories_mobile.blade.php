0d728dd74ec263e87c023b19c30dcb6e5466653a
Carlos Maita <Carlosmaita2009@gmail.com>
2021-02-14 01:35:45 -0400

    Ajustes de menu

--- resources/views/common/header/menuCategories_mobile.blade.php
+++ resources/views/common/header/menuCategories_mobile.blade.php
@@ -7,17 +7,20 @@
             </div>
         </div>
         <div class="categorie__mobileBody">
-            @foreach($categorias as $categoria)
-            <a href="{{route('product.category.show', $categoria->slug)}}" class="categorie__mobileBody-item">
-                <span class="categorie__mobileBody-icon">
-                    @include('common.header.menuCategoryIcon_mobile')    
-                </span>
-                <span class="categorie__mobileBody-iconActive">
-                    @include('common.header.menuCategoryIconActive_mobile')
-                </span>
-                {{$categoria->title}}
-            </a>
-            @endforeach
+            @for ($i = 0; $i < 7; $i++)
+                @foreach($categorias as $categoria)
+                <a href="{{route('product.category.show', $categoria->slug)}}" class="categorie__mobileBody-item">
+                    <span class="categorie__mobileBody-icon">
+                        @include('common.header.menuCategoryIcon_mobile')    
+                    </span>
+                    <span class="categorie__mobileBody-iconActive">
+                        @include('common.header.menuCategoryIconActive_mobile')
+                    </span>
+                    {{$categoria->title}}
+                </a>
+                @endforeach
+                
+            @endfor
         </div>
     </div>
 @endisset
