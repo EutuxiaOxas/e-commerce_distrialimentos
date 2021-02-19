e368d6942dba7c3ab944f0efea4b6c3329046579
Carlos Maita <Carlosmaita2009@gmail.com>
2021-02-19 15:14:13 -0400

    Ajutes de modal de detalle

--- resources/views/perfil/compras.blade.php
+++ resources/views/perfil/compras.blade.php
@@ -30,7 +30,7 @@
                             @if($orden->status_id =='1' )
                              <a href="#" class="perfil__comprasActions-cancelar">Cancelar compra</a>
                              @endif
-                             <a href="#" class="perfil__comprasActions-detalle"> Ver detalle</a>
+                             <a data-toggle="modal" data-target="#modal_DetalleOrden" class="perfil__comprasActions-detalle"> Ver detalle</a>
                         </div>
                     </div>
                     {{--  Fin Por pagar --}}
@@ -45,4 +45,85 @@
             </div>
         </div>
     </section>
+    
+    
+    {{-- modal de detalles --}}
+	<div class="modal fade" id="modal_DetalleOrden" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content p-3">
+                {{-- Header del modal --}}
+				<div class="modal-header p-2 border-bottom-0">
+					<div class="container">
+						<div class="row">
+						<div class="col-10 mb-0">
+							<h4 class="text-secondary font-weight-bold" id="exampleModalLabel">Detalle de pedido</h4>
+							<p class="texto-small texto-muted">Este pedido fue completado con exito...</p>
+						</div>
+						<div class="col-2">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>        
+						</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="modal-body px-2 pt-1 pb-2">
+                    {{-- datos de pedido --}}
+					<div class="form-title container pb-2">
+						<div class="row">
+						<div class="col text-left">
+							<h5 class="font-weight-bold">Datos de pedido</h5>
+						</div>
+						</div>
+					</div>
+                    <div class="form">
+                        <div class="col">
+                            <p class="text-muted"><b>ID:</b> 000000254</p>
+                            <p class="text-muted"><b>Fecha:</b> 000000254</p>
+                            <p class="text-muted"><b>Ultima modificacion:</b> 000000254</p>
+                            <p class="text-muted"><b>Monto:</b> 000000254</p>
+                        </div>
+                    </div>
+                    {{-- datos de envio --}}
+                    <div class="form-title container pb-2">
+						<div class="row">
+						<div class="col text-left">
+							<h5 class="font-weight-bold">Datos de pedido</h5>
+						</div>
+						</div>
+					</div>
+                    <div class="perfil__cardDireccion">
+                        <p class="text-muted"><b>Calle 1 Avenida 10 Local 45</b></p>
+                        <p class="text-muted">Carabobo, Valencia (2001)</p>
+                        <p class="text-muted">Carlos Maita - +58 414 453 3456</p>
+                    </div>
+                    <div class="form-title container pb-2">
+						<div class="row">
+						<div class="col text-left">
+							<h5 class="font-weight-bold">Rutas de entrega</h5>
+						</div>
+						</div>
+					</div>
+                    <div class="form">
+                        <div class="col">
+                            <p class="text-muted"><b>Ruta:</b> Valencia, Lunes 08:00 AM</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="form-title container pb-2">
+						<div class="row">
+						<div class="col text-left">
+							<h5 class="font-weight-bold">Lista de Productos</h5>
+						</div>
+						</div>
+					</div>      
+                    
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- Fin Modal datos de empresa -->
+
 @endsection
