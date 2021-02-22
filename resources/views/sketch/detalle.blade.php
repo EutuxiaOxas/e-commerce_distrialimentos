6914f5451f5f37b93c78c4ba995350e6c9b05d7b
Mariana Lamus <marilams995@gmail.com>
2021-02-22 14:35:31 -0400

    detalles

--- resources/views/sketch/detalle.blade.php
+++ resources/views/sketch/detalle.blade.php
@@ -103,6 +103,9 @@
         padding-top: 0.3rem;
       }
     }
+    .fs-14 {
+      font-size: 0.75rem;
+    }
    </style>
    <!-- Prdoduct -->
     <section class="pb-3">
@@ -168,7 +171,7 @@
                             {{session('currency') == 'USD' ? '$' : 'Bs'}} 
                             {{$product->getPrice(session('currency'), $product->wholesale_price)}} 
                           </h3>
-                          <p class="productDetail__priceDescription-unitPrice">
+                          <p class="productDetail__priceDescription-unitPrice fs-14">
                             {{$product->getPrice(session('currency'), $alMayorUnitPrice)}}  
                             {{session('currency') == 'USD' ? '$' : 'Bs'}}
                             / unidad
@@ -189,7 +192,7 @@
                           {{session('currency') == 'USD' ? '$' : 'Bs'}}
                           {{$product->getPrice(session('currency'), $product->big_wholesale_price)}} 
                         </h3>
-                        <p class="productDetail__priceDescription-unitPrice normal">
+                        <p class="productDetail__priceDescription-unitPrice normal fs-14">
                           {{$product->getPrice(session('currency'), $alGranMayorUnitPrice)}}  
                           {{session('currency') == 'USD' ? '$' : 'Bs'}}
                           / unidad
