bb8abc896559217bc2171626d39efe9e244c8023
Carlos Maita <Carlosmaita2009@gmail.com>
2021-02-25 08:35:48 -0400

    ajustes

--- resources/views/common/header/header_desktop.blade.php
+++ resources/views/common/header/header_desktop.blade.php
@@ -72,10 +72,10 @@
 					<a href="{{route('almacen.all')}}">Almacen</a>
 				</li>
 				<li class="navbar__list-item">
-					<a href="{{route('almacen.all')}}">Productos recientes</a>
+					<a href="{{route('almacen.all')}}">Más recientes</a>
 				</li>
 				<li class="navbar__list-item">
-					<a href="{{route('almacen.all')}}">Mas vendidos</a>
+					<a href="{{route('almacen.all')}}">Destacados</a>
 				</li>
 				<li class="navbar__list-item">
 					<a href="{{route('ayuda')}}">Ayuda</a>
