959bff2c86b9caf4ccaeb7e79da91033508e60c9
Andrés Gazui <gazuiandres@gmail.com>
2021-03-02 11:41:41 -0400

    formulario pago

--- resources/views/formulario/datos-pago-dos.blade.php
+++ resources/views/formulario/datos-pago-dos.blade.php
@@ -1,213 +1,50 @@
-<section id="formulario1" class="formularios__sections show">
+<div class="hideForm" id="formPaymentToFinish">
+  <div class="formPayment__headerMain">
+    <div class="formPayment__headerLeft">
+      <h5 class="formPayment__headerMain-title" id="paymentForm__title">Zelle</h5>
+      <p class="formPayment__headerMain-description" id="paymentForm__description">Transferencia en Dolares</p>
+    </div>
+    <div class="formPayment__headerRight">
+      <h5 class="formPayment__headerMain-title price" id="paymentForm__totalAmount">20000.00 $</h5>
+      <p class="formPayment__headerMain-description price" id="paymentForm__amountDescription">Monto a transferir</p>
+    </div>
+  </div>
 
-  <!-- datos de usuario-->                                
-  <section class="py-1">
-    <!-- Number-->
-    <div class="container">
-      <div class="row p-1">
-        <div class="col-2 pt-1 pr-0 col-md-1 col-lg-1">
-          <button class="formularios__btnNumber bg-primary">
-            <p class="text-white small">1</p>
-          </button>
+  <div class="alert formPayment__alertInformation" role="alert">
+      <div id="formPayment__alertInformationContainer">
+        <div class="formPayment__alertInformation-actionContainer">
+          <p class="formPayment__alertInformation-paymentData">Transferir a: </p>
         </div>
-        <div class="col-10 d-flex align-items-center pl-0 col-md-11 col-lg-11">
-          <p class="font-weight-bold texto-small">Datos de pago</p>
-        </div>
-      </div>
-    </div>
-    <!-- fin Number-->
-      <!--  datos de usuario-->
-    <div class="container mt-1 px-3">
-      <div class="border shadow" id="formUserInfoContainer">
-        <div class="info-container p-3">
-          
-          <div class="row">
-            <div class="col-12 text-center pt-3 padding_modal">
-              <a href="#" class="texto-small font-weight-bold text-secondary" data-toggle="modal" data-target="#modal-user-edit">Editar datos de comprador</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /datos de usuario-->
-  </section>
-    
-  <!-- Buttoms-->
-  <section class="my-0 pt-3">
-    <div class="container">
-      <div class="row">
-        <div class="col-5 pr-0">
-          <button type="button" class="btn btn-sm btn-block formularios__btn-left" disabled><span></span></button>
-        </div>
-        <div class="col-7 pl-0">
-          <button id="btn_next1" class="btn btn-primary btn-sm btn-block formularios__btn-right" @if( !($user->documento_identidad && $user->phone) ) disabled @endif >Siguiente</button>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- /Buttoms-->
-                    
-</section>
 
+        <div>
+          <p class="formPayment__alertInformation-paymentData"><span>Correo zelle:</span> admin@admin.com</p>
+          <p class="formPayment__alertInformation-paymentData"><span>Titular:</span> Administrador </p>
+        </div>
+      </div>
+  </div>
 
-{{-- Modales --}}
-<!-- Modal datos de usuario -->
-<div class="modal fade" id="modal-user-edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content p-3">
-      <div class="modal-header p-2 border-bottom-0">
-        <div class="container">
-          <div class="row">
-            <div class="col-10 mb-0">
-              <h5 class="modal-title text-secondary font-weight-bold lead" id="exampleModalLabel">Datos de usuario</h5>
-              <p class="texto-small text-muted">Agregue los datos solicitados...</p>
-            </div>
-            <div class="col-2">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>        
-            </div>
-          </div>
-        </div>
+  <div class="formPayment__paymentInfomation">
+    <form action="">
+      @csrf
+      <div class="login__inputContainer">
+        <input type="text" class="login__inputContainer-input" name="titular_cuenta" placeholder="Titular de cuenta" autocomplete="off">
       </div>
-      <div class="modal-body px-2 pt-1 pb-2">
-        <div class="form-title container">
-          <div class="row">
-            <div class="col text-center">
-              <p class="font-weight-bold">Datos de usuario</p>
-            </div>
-          </div>
-        </div>
-        <form action="{{route('user.info.update')}}" id="formEditOrCreateUserInfo" method="POST">
-          @csrf
-          <div class="form">
-            <div class="col">
-              <input type="text" class="form-control-plaintext formularios__inputBorders" value="{{$user->name ?? ''}}" name="name" placeholder="Nombre completo">
-            </div>
-            <div class="col">
-              <input type="text" class="form-control-plaintext formularios__inputBorders" value="{{$user->documento_identidad ?? ''}}" name="documento_identidad"  placeholder="Cedula [ej: v-23432578]">
-            </div>
-            <div class="col">
-              <input type="text" class="form-control-plaintext formularios__inputBorders" value="{{$user->phone ?? ''}}"  name="phone" placeholder="Telefono [ej: 0241-8524234]">
-            </div>
-            <div class="col pb-3">
-              <input type="text" class="form-control-plaintext formularios__inputBorders" value="{{$user->phone_alternative ?? ''}}" name="phone_alternative" placeholder="Telefono Alt. [ej: 0241-8524234]">
-            </div>
-          </div>
-          <div class="container">
-            <div class="row mb-1">
-              <button type="submit" class="btn btn-primary btn-block formulario__modalBtn">
-                @if($user->documento_identidad && $user->phone)
-                  Editar
-                @else 
-                  Agregar
-                @endif
-              </button>
-            </div>
-            <div class="row">
-              <p class="text-muted texto-small text-center">Al hacer click en continuar usted confirma que los datos administrados son reales</p>
-            </div>
-          </div>
-        </form>
+
+      <div class="login__inputContainer">
+        <input type="text" class="login__inputContainer-input" name="documento_identidad_titular" placeholder="Documento identidad titular" autocomplete="off">
       </div>
+
+      <div class="login__inputContainer">
+        <input type="text" class="login__inputContainer-input" name="referencia" placeholder="Referencia" autocomplete="off">
+      </div>
+
+      <div class="login__inputContainer">
+        <input type="number" class="login__inputContainer-input" name="monto" placeholder="Monto pagado" autocomplete="off">
+      </div>
+    </form>
+    <div class="fomularioMetodoPago__metodoAlert">
+      <p class="fomularioMetodoPago__metodoAlert-text">Inserte datos del pago</p>
     </div>
-  </div>  
+  </div>
+
 </div>
-
-<!-- Fin Modal datos de usuario -->
-
-<script>
-  function compraFormBuyer(){
-
-    function addLoader(container) {
-      element = '<div class="loader">Loading...</div>';
-      container.innerHTML = element;
-    }
-
-    function addUserInfoToView(container, {
-      name,
-      documento_identidad,
-      phone,
-      phone_alternative
-    }) {
-      const view = `
-      <div class="info-container p-3">
-          <div class="row mb-3">
-            <div class="col-6">
-              <p class="font-weight-bold texto-small">Nombre y apellido</p>
-            </div>
-            <div class="col-6">
-              <p class="texto-small text-muted text-md-right">${name}</p>
-            </div>
-          </div>
-          <div class="row mb-3">
-            <div class="col-6">
-              <p class="font-weight-bold texto-small">Documento</p>
-            </div>
-            <div class="col-6">
-              <p class="texto-small text-muted text-md-right">C.I. ${documento_identidad}</p>
-            </div>
-          </div>
-          <div class="row mb-3">
-            <div class="col-6">
-              <p class="font-weight-bold texto-small">Telefono</p>
-            </div>
-            <div class="col-6">
-              <p class="texto-small text-muted text-md-right">${phone}</p>
-            </div>
-          </div>
-          <div class="row mb-3">
-            <div class="col-6">
-              <p class="font-weight-bold texto-small">Telefono alternativo</p>
-            </div>
-            <div class="col-6">
-              <p class="texto-small text-muted text-md-right">${phone_alternative ? phone_alternative : '-'}</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 text-center pt-3 padding_modal">
-              <a href="#" class="texto-small font-weight-bold text-secondary" data-toggle="modal" data-target="#modal-user-edit">Editar datos de comprador</a>
-            </div>
-        </div>
-      </div>
-      `
-      container.innerHTML = view;
-    }
-
-    const formUserInfo = document.getElementById('formEditOrCreateUserInfo');
-    const userContainer = document.getElementById('formUserInfoContainer');
-    const $btn_next1 = document.getElementById('btn_next1');
-
-    formUserInfo.addEventListener('submit', (e) => {
-      e.preventDefault();
-
-      // form data
-      const [, name, documentoIdentidad, phone, phoneAlternative] = e.target;
-
-      addLoader(userContainer);
-
-      //close modal
-      $('#modal-user-edit').modal('hide');
-
-      axios.post(`/perfil/userInfo`, {
-        name: name.value,
-        documento_identidad: documentoIdentidad.value,
-        phone: phone.value,
-        phone_alternative: phoneAlternative.value
-      })
-      .then(res => {
-        setTimeout(() => {
-          addUserInfoToView(userContainer, res.data)
-          $btn_next1.removeAttribute('disabled')
-        }, 2000);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-    })
-  }
-
-  // function scope
-  compraFormBuyer();
-</script>
--- resources/views/formulario/datos-pago-uno.blade.php
+++ resources/views/formulario/datos-pago-uno.blade.php
@@ -20,24 +20,30 @@
     <div class="container mt-1 px-3">
       <div class="border shadow" id="formUserInfoContainer">
         <div class="info-container p-3">
-            @foreach($banks as $bank)
-              <div class="fomularioMetodoPago__selectbBankCard">
-                  <div class="fomularioMetodoPago__selectbBankCardIcon">
-                    @if($bank->title == 'Zelle')
-                      <img src="{{asset('images/bank_zelle.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-inactive" alt="Icono banco">
-                    @elseif($bank->title == 'Efectivo')
-                      <img src="{{asset('images/bank_dolar.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-inactive" alt="Icono banco">
-                    @else
-                      <img src="{{asset('images/bank_icon_inactive.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-inactive" alt="Icono banco">
-                      <img src="{{asset('images/bank_icon_active.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-active" alt="Icono banco">
-                    @endif
-                  </div>
-                  <div class="fomularioMetodoPago__selectbBankCardBody">
-                    <h3 class="fomularioMetodoPago__selectbBankCardBody-title">{{$bank->title ?? ''}}</h3>
-                    <p class="fomularioMetodoPago__selectbBankCardBody-description">{{$bank->description ?? ''}}</p>
-                  </div>
+            <div class="" id="selectPaymentMethodSection">
+              @foreach($banks as $bank)
+                <div class="fomularioMetodoPago__selectbBankCard payment_type" data-id="{{$bank->id}}" data-titular="{{$bank->titular}}" data-account="{{$bank->number_account}}">
+                    <div class="fomularioMetodoPago__selectbBankCardIcon ">
+                      @if($bank->title == 'Zelle')
+                        <img src="{{asset('images/bank_zelle.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-inactive" alt="Icono banco">
+                      @elseif($bank->title == 'Efectivo')
+                        <img src="{{asset('images/bank_dolar.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-inactive" alt="Icono banco">
+                      @else
+                        <img src="{{asset('images/bank_icon_inactive.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-inactive" alt="Icono banco">
+                        <img src="{{asset('images/bank_icon_active.png')}}" class="fomularioMetodoPago__selectbBankCardIcon-active" alt="Icono banco">
+                      @endif
+                    </div>
+                    <div class="fomularioMetodoPago__selectbBankCardBody">
+                      <h3 class="fomularioMetodoPago__selectbBankCardBody-title">{{$bank->title ?? ''}}</h3>
+                      <p class="fomularioMetodoPago__selectbBankCardBody-description">{{$bank->description ?? ''}}</p>
+                    </div>
+                </div>
+              @endforeach
+              <div class="fomularioMetodoPago__metodoAlert">
+                <p class="fomularioMetodoPago__metodoAlert-text">Seleccion un método de pago.</p>
               </div>
-            @endforeach
+            </div>
+            @include('formulario.datos-pago-dos')
         </div>
       </div>
     </div>
@@ -64,5 +70,38 @@
 <!-- Fin Modal datos de usuario -->
 
 <script>
-  
+    function paymentMethodForm() {
+
+      function toggleChooseMethodToPaymentForm(){
+        const selectPaymentForm = document.getElementById('selectPaymentMethodSection');
+        const paymentForm = document.getElementById('formPaymentToFinish');
+        
+        
+        selectPaymentForm.classList.toggle('hideForm');
+        formPaymentToFinish.classList.toggle('hideForm');
+      }
+
+
+      const metodoPago = document.querySelectorAll('.payment_type');
+
+      if(metodoPago) {
+        metodoPago.forEach(metodo => {
+          metodo.addEventListener('click', (e) => {
+            let container = e.target
+            
+            if(e.target.tagName !== 'DIV') {
+              container = e.target.parentNode.parentNode;
+            }
+
+            const idPyamentMethod = container.dataset.id;
+            const titularPaymentMethod = container.dataset.titular;
+            const accountPaymentMethod = container.dataset.account;
+
+            toggleChooseMethodToPaymentForm()
+          })
+        })
+      }
+    }
+
+    paymentMethodForm();
 </script>
