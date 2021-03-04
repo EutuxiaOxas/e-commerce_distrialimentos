1426d6ce9fccf4f1bf1e5f6193bbb0876b8fb043
Carlos Maita <Carlosmaita2009@gmail.com>
2021-03-04 14:08:20 -0400

    Ajustes de precio de delivery por ciudad

--- database/seeds/CitySeeder.php
+++ database/seeds/CitySeeder.php
@@ -16,31 +16,40 @@ class CitySeeder extends Seeder
             'id' => 1,
         	'state_id' => '7',
         	'city' => 'Valencia',
-        	'delivery_time' => '1 día'
+        	'delivery_time' => '1 día',
+        	'delivery_price' => 3.00
         ]);
         DB::table('cities')->insert([
             'id' => 2,
         	'state_id' => '7',
         	'city' => 'Naguanagua',
-        	'delivery_time' => '3 días'
+        	'delivery_time' => '3 días',
+        	'delivery_price' => 3.00
+
         ]);
         DB::table('cities')->insert([
             'id' => 3,
         	'state_id' => '7',
         	'city' => 'San Diego',
-        	'delivery_time' => '3 días'
+        	'delivery_time' => '3 días',
+        	'delivery_price' => 3.00
+
         ]);
         DB::table('cities')->insert([
             'id' => 4,
         	'state_id' => '7',
         	'city' => 'Puerto Cabello',
-        	'delivery_time' => '3 días'
+        	'delivery_time' => '3 días',
+        	'delivery_price' => 3.00
+
         ]);
         DB::table('cities')->insert([
             'id' => 5,
         	'state_id' => '4',
         	'city' => 'Maracay',
-        	'delivery_time' => '3 días'
+        	'delivery_time' => '3 días',
+        	'delivery_price' => 3.00
+
         ]);
     }
 }
